b9895b46c72cf983158b997803d8517b857c16ef
roboblox <supeecyborg@gmail.com>
2020-06-08 18:05:35 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,39 +1,77 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
-function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+function App(props) {
+  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
 
+  const homeTouchdown = (evt) => {
+    setHomeScore(homeScore + 7);
+  };
+  const awayTouchdown = (evt) => {
+    setAwayScore(awayScore + 7);
+  };
+  const homeFieldGoal = (evt) => {
+    setHomeScore(homeScore + 3);
+  };
+  const awayFieldGoal = (evt) => {
+    setAwayScore(awayScore + 3);
+  };
+
+  // const scores = (evt) => {
+  //   if (homeFieldGoal) {
+  //     setHomeScore(homeScore + 3);
+  //   } else if (awayFieldGoal) {
+  //     setHomeScore(awayScore + 3);
+  //   } else if (homeTouchdown) {
+  //     setHomeScore(homeScore + 7);
+  //   } else if (awayTouchdown) {
+  //     setAwayScore(awayScore + 7);
+  //   }
+  // };
+
+  // work={workToIncreaseSavings} name="Cameron" account={savings / 2}
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
+            {/* TODO STEP 3 - We need to change the hardcoded values in these
+            divs to accept dynamic values from our state. */}
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
         <div className="homeButtons">
-
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={homeTouchdown} className="homeButtons__touchdown">
+            Home Touchdown
+          </button>
+          <button onClick={homeFieldGoal} className="homeButtons__fieldGoal">
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          {
+            <button onClick={awayTouchdown} className="awayButtons__touchdown">
+              Away Touchdown
+            </button>
+          }
+          <button onClick={awayFieldGoal} className="awayButtons__fieldGoal">
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
